094aa9ee5f852c228cbae048edd2e78d03ce7333
maxyee <eyaminkhan00@gmail.com>
2022-07-17 14:28:57 +0100

    added the student configaration for adding demo data to database

---
+++ demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,36 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student Max = new Student(
+                    "Max",
+                    "max@gmail.com",
+                    LocalDate.of(2000, Month.APRIL, 5),
+                    21
+            );
+
+
+            Student Ali = new Student(
+                    "Ali",
+                    "ali@gmail.com",
+                    LocalDate.of(2002, Month.APRIL, 6),
+                    19
+            );
+
+            repository.saveAll(
+                    List.of(Max, Ali)
+            );
+        };
+    }
+}
